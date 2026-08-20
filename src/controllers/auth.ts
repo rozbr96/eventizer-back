87ca98c03d0ad349c81a2a0888712a2ae53527bb
Rosemilson <rozbr96@gmail.com>
2026-08-20 13:23:44 -0300

    feat: create user first token upon signing up

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,12 +4,20 @@ import type { Request, Response } from 'express'
 import { CreateUserUseCase } from '@/core/use-cases/user/index.js'
 import UserRepository from '@/prisma/repositories/user.js'
 
+import { CreateUserTokenUseCase } from '@/core/use-cases/index.js'
+import UserTokenRepository from '@/redis/repositories/user-token.js'
+
 const signup = (req: Request, resp: Response) => {
   const userRepository = new UserRepository()
+  const userTokenRepository = new UserTokenRepository()
 
   new CreateUserUseCase(userRepository)
     .execute(req.body)
-    .then(() => { resp.end() })
+    .then(() => {
+      new CreateUserTokenUseCase(userTokenRepository)
+        .execute({ email: req.body.email })
+        .then(() => { resp.end() })
+    })
     .catch((err) => { resp.status(400).json(err) })
 }
 
