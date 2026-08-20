cfc6b0c1340016b2b6b4a13bb539f4512513109d
Rosemilson <rozbr96@gmail.com>
2026-08-20 18:33:48 -0300

    feat: improve token generation

--- src/core/use-cases/user-token/create.ts
+++ src/core/use-cases/user-token/create.ts
@@ -1,10 +1,12 @@
 
-import crypto from 'crypto'
+import * as jwt from 'jsonwebtoken'
 
 import type { UserTokenRepository } from '@/core/repositories/index.js'
 
 interface CreateUserTokenUseCaseProps {
+  name: string
   email: string
+  role: string
 }
 
 export class CreateUserTokenUseCase {
@@ -12,7 +14,7 @@ export class CreateUserTokenUseCase {
 
   execute(props: CreateUserTokenUseCaseProps) {
     return new Promise<string>(async (resolve, reject) => {
-      const token = crypto.randomBytes(32).toString('hex')
+      const token = jwt.sign(props, process.env.JWT_SECRET!)
 
       this.repository
         .set(props.email, token, 3600)
