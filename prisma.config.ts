c3066e8a27041e7c9d94724025ce9fad5d773ce8
Rosemilson <rozbr96@gmail.com>
2026-08-20 05:23:56 -0300

    chore: change db setup

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,14 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS } = process.env
+
 export default defineConfig({
   schema: "src/prisma/schema.prisma",
   migrations: {
     path: "src/prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: `postgresql://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   },
 });
