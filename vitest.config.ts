32fa00d3bb49890c18d599acb3ea623416618413
Rosemilson <rozbr96@gmail.com>
2026-08-20 15:51:25 -0300

    chore: disable test files paralelism

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  resolve: { tsconfigPaths: true }
+  resolve: { tsconfigPaths: true },
+  test: {
+    fileParallelism: false
+  }
 })
 
