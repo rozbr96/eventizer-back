53ae0b3f9a89e6ab7ea749bf83606746ee7da97e
Rosemilson <rozbr96@gmail.com>
2026-08-22 08:42:41 -0300

    chore: restrict tests to files under test folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   resolve: { tsconfigPaths: true },
   test: {
+    include: ['test/**\/*.{test,spec}.?(c|m)[jt]s?(x)'],
     fileParallelism: false,
     setupFiles: ['test/setup.ts']
   }
