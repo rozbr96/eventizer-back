48e36123db060806de84f51c32f7d344c26826eb
Rosemilson <rozbr96@gmail.com>
2026-08-23 06:17:34 -0300

    feat: VerifyTicketUseCase

--- src/core/repositories/ticket.ts
+++ src/core/repositories/ticket.ts
@@ -1,12 +1,14 @@
 
-import type { TicketRetrieval } from '@/core/entities/index.js'
+import type { TicketEdition, TicketRetrieval } from '@/core/entities/index.js'
 
 interface TicketCreationProps {
   purchase_id: number
   event_id: number
   holder: string
 }
-export default abstract class TicketRepository<EventMetadata> {
+export default abstract class TicketRepository<EventMetadata = any> {
   abstract create(props: TicketCreationProps): Promise<TicketRetrieval<EventMetadata>>
+  abstract update(code: string, props: TicketEdition): Promise<TicketRetrieval<EventMetadata>>
+  abstract findByCode(code: string): Promise<TicketRetrieval<EventMetadata> | null>
 }
 
