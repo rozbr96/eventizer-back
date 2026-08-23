95b206e06f24ad54ab6ee3e1980a1304b31d6f38
Rosemilson <rozbr96@gmail.com>
2026-08-23 04:34:24 -0300

    feat: Ticket

---
+++ src/core/entities/ticket.ts
@@ -0,0 +1,23 @@
+
+import type {
+  EventRetrieval,
+  PurchaseRetrieval
+} from '@/core/entities/index.js'
+
+export interface TicketCreation {
+  holder: string
+  purchase_id: number
+  event_id: number
+}
+
+export interface TicketRetrieval<EventMetadata> extends TicketCreation {
+  id: number
+  code: string
+  purchase: PurchaseRetrieval<EventMetadata>
+  event: EventRetrieval<EventMetadata>
+}
+
+export interface TicketEdition {
+  consumed?: boolean
+}
+
